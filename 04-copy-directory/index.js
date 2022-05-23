842fc249a639a6e234373ab979851f1ec4fb69b0
DoroPomidoro <pomidorodoro@gmail.com>
2022-05-24 00:54:26 +0300

    fix: change solve 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,25 +3,26 @@ const path = require('path');
 const currentFolder = path.join(__dirname, './files/');
 const copyFolder = path.join(__dirname, './files-copy/');
 fs.mkdir(copyFolder, { recursive: true }, error => {
-  if (error) {
-    throw error;
-  } else {
-    fs.readdir(currentFolder, (err, files) => {
-      if (err)
-        console.log(err);
-      else {
-        fs.rmdir(copyFolder, () => {// удаляем содержимое новой созданной папки
-          console.log('Folder Deleted!');
-        });
-        files.forEach(file => {
-          fs.mkdir(copyFolder, { recursive: true }, error => {
-            if (error) throw error;
-            fs.copyFile(path.join(currentFolder, file), path.join(copyFolder, file), err => {//копируем содержимое старой папки в новую
-              if(err) throw err;
-            });
-          });
-        });
-      }
+  if (error) throw error;
+  fs.readdir(copyFolder, { recursive: true }, (err, files) => {
+
+    if (err) throw error;   
+    files.forEach(file => {
+      fs.unlink(path.join(copyFolder, file), err => {
+        if(err) throw err;
+      });
     });
-  }
+  }),
+
+  fs.readdir(currentFolder, { recursive: true }, (err, files) => { 
+    if (err) throw error;
+    files.forEach(file => {
+      fs.mkdir(copyFolder, { recursive: true }, error => {
+        if (error) throw error;
+        fs.copyFile(path.join(currentFolder, file), path.join(copyFolder, file), err => {//копируем содержимое старой папки в новую
+          if(err) throw err;
+        });
+      });
+    });  
+  });
 });
